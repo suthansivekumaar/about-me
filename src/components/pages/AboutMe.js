7bee7fa1411facf91a5627a7403aef10acff8d65
Suthan <suthan.sivekumaar@gmail.com>
2022-12-24 02:59:33 -0500

    Updated code and added latest co-op position

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -4,19 +4,22 @@ import '../../App.css';
 export default function AboutMe() {
   return (
   <div>
-    <h2>ABOUT ME</h2>
+    <h2 id="about-me-heading">ABOUT ME</h2>
+    <img id="profile" src={require("../images/Profile.jpg")} alt="profile"/>
     <div className='aboutMe'>
-      Welcome to my website built using React.
-      <br></br><br></br>
-      My name is Suthan Sivekumaar and I am a 4th year Management Engineering student at the University of Waterloo. I am currently seeking full-time job
-  opportunities for Summer or Fall 2023. I have a passion for programming and learning new technology. Please view my GitHub profile to see all my projects. 
-  <br></br><br></br>
-  Feel free to contact me through LinkedIn or email.</div>
-  <img id="profile" src={require("../images/Profile.jpg")} alt="Pofile" width="300" height="400"/>
-    <div>
-      <a href="https://github.com/suthansivekumaar"><img id="github" src={require("../images/GitHub.jpg")} alt="GitHub" width="100" height="100"/></a>
-      <a href="mailto:ssivekum@uwaterloo.ca"><img id="email" src={require("../images/Email.jpg")} alt="Email" width="100" height="100"/></a>
-      <a href="https://www.linkedin.com/in/suthan-sivekumaar"><img id="linkedin" src={require("../images/LinkedIn.png")} alt="LinkedIn" width="70" height="70"/></a>
+      <div className='welcomeMessage'>
+        Welcome to my website built using React.
+      </div>
+      <div className='introduction'>
+        My name is Suthan Sivekumaar and I am a 4th year Management Engineering student at the University of Waterloo. I am currently seeking full-time job
+        opportunities for Summer or Fall 2023. I have a passion for programming and learning new technology. Please view my GitHub profile to see all my projects. 
+        Feel free to contact me through LinkedIn or email.
+      </div>
+  </div>
+    <div className='pictures'>
+      <a href="https://github.com/suthansivekumaar"><img id="github" className="picture" src={require("../images/GitHub.jpg")} alt="GitHub" width="100" height="100"/></a>
+      <a href="mailto:ssivekum@uwaterloo.ca"><img id="email" className="picture" src={require("../images/Email.jpg")} alt="Email" width="100" height="100"/></a>
+      <a href="https://www.linkedin.com/in/suthan-sivekumaar"><img id="linkedin" className="picture" src={require("../images/LinkedIn.png")} alt="LinkedIn" width="70" height="70"/></a>
     </div>
   </div>
   );
--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -4,54 +4,80 @@ import '../../App.css';
 export default function Experience() {
   return (
     <div>
-    <h2>EXPERIENCE</h2>
+    <h2 id="experience-heading">EXPERIENCE</h2>
         <div class="entire-block">
         <ul className='work-experience'>
             <li className='work-experience-coop'>
-                <div className='work-experience-company'>Marsh McLennan</div>
-                <div className='work-experience-position'>Software Developer</div>
-                <div className='work-experience-dates'>Jan 2022 to Apr 2022</div>
-                <div className='work-experience-description'>
-                <p> - Added missing fields to Contract Lifecycle Manager and Legal Matter Intake Tool based on feedback from end users
-                <br></br> - Added email-sending process after change requests were submitted on Contract Lifecycle Manager to notify the requester if the change request was increase type or decrease type
-                <br></br> - Improved the team's testing process for Request Access, Vaccination Tracking and Ukraine/Russia Sanctions, by creating automated test cases using the CodeCeptJS testing framework
-                <br></br> - Created a prototype of a web app that tracks the working location of each employee using MEAN stack and Electron for the MMC Tech Hackathon and pitched solution to judges
-                    </p>
-                </div>
+                <span className='work-experience-company'>Loblaw Digital</span>
+                <span className='work-experience-location'>Toronto, ON, Canada</span>
+                <span className='work-experience-position'>Software Developer</span>
+                <span className='work-experience-dates'>Sep 2022 to Dec 2022</span>
+                <ul className='work-experience-description'>
+                    <li>
+                        Implemented URL canonicalization for Loblaw’s product list pages, product detail pages, collection pages and store locator details pages 
+                        through React, removing over 291,000 non-canonical pages from Google and improving SEO performance
+                    </li>
+                    <li>
+                        Created modal through React and CSS that appears if a customer’s delivery order can be fulfilled through Loblaw’s manual micro fulfillment
+                        centers, improving order experience by allowing customers to select their current delivery timeslot or 2 faster timeslots
+                    </li>
+                    <li>
+                        Added store ID and postal code as parameters to delivery REST API, allowing experimentation team to select them as parameters in the custom 
+                        attribute when creating an audience in Adobe Target and helping to track metrics from different store locations
+                    </li>
+                </ul>
             </li>
-            <br></br>
             <li className='work-experience-coop'>
-                <div className='work-experience-company'>Fundscraper Capital</div>
-                <div className='work-experience-position'>Software Developer</div>
-                <div className='work-experience-dates'>May 2021 to Aug 2021</div>
-                <div className='work-experience-description'>
-                    <p> - Converted PHP code part of the company’s order process into TypeScript and ran tests using Jest to verify the calculations surrounding real-estate transactions in the new code are correct
-                    <br></br> - Verified correct email output by converting email string code for each method involved with email sending after each transaction into Handlebars expressions and ran tests using PHPUnit
-                    </p>
-                </div>
+                <span className='work-experience-company'>Marsh McLennan</span>
+                <span className='work-experience-location'>Toronto, ON, Canada</span>
+                <span className='work-experience-position'>Software Developer</span>
+                <span className='work-experience-dates'>Jan 2022 to Apr 2022</span>
+                <ul className='work-experience-description'>
+                    <li>
+                        Created a prototype of a web app that tracks the working location of 80,000 employees using MERN stack and Electron, helping departments 
+                        with payroll administration
+                    </li>
+                    <li>
+                        Designed email-sending process after change requests were submitted on Contract Lifecycle Manager to notify the requester if the change 
+                        request was increase type or decrease type using PEGA
+                    </li>
+                    <li>
+                        Improved the team’s testing process for Request Access, Vaccination Tracking and Ukraine/Russia Sanctions, by creating 143 automated test
+                        cases using the CodeceptJS testing framework
+                    </li>
+                </ul>
             </li>
-            <br></br>
             <li className='work-experience-coop'>
-                <div className='work-experience-company'>Bank of Montreal</div>
-                <div className='work-experience-position'>Operations Analyst</div>
-                <div className='work-experience-dates'>Sep 2020 to Dec 2020</div>
-                <div className='work-experience-description'>
-                    <p> - Updated webpage used for SAP S/4HANA training resources using HTML and CSS, improving user experience by adding more resources and making page auto-scalable
-                    <br></br> - Created a General Ledger Account search engine portal using HTML, CSS and JavaScript
-                    <br></br> - Created a tool that allows users with limited HTML experience to edit the Notepad file of the SAP S/4HANA Training webpage and was nominated "Star of the Week" for it
-                    <br></br> - Collaborated with team to propose a solution that will help BMO be at the forefront of change during COVID-19 and pitched solution to judges where our team won</p>
-                </div>
+                <span className='work-experience-company'>Fundscraper Capital</span>
+                <span className='work-experience-location'>Toronto, ON, Canada</span>
+                <span className='work-experience-position'>Software Developer</span>
+                <span className='work-experience-dates'>May 2021 to Aug 2021</span>
+                <ul className='work-experience-description'>
+                    <li>
+                        Converted PHP code part of the company’s order process into TypeScript and ran tests using Jest to verify the calculations surrounding real-estate transactions in the new code are correct
+                    </li>
+                     <li>
+                        Verified correct email output by converting email string code for each method involved with email sending after each transaction into Handlebars expressions and ran tests using PHPUnit
+                     </li>
+                </ul>
             </li>
-            <br></br>
             <li className='work-experience-coop'>
-                <div className='work-experience-company'>ATS Automation</div>
-                <div className='work-experience-position'>Software Toolset Developer</div>
-                <div className='work-experience-dates'>Jan 2020 to Apr 2020</div>
-                <div className='work-experience-description'>
-                <p> - Resolved team’s runtime errors and compile errors in their Excel files by updating obsolete VBA code and using breakpoints 
-                    <br></br> - Automated data input and data output across several sheets in General Process Summary with VBA, macros, VLOOKUP, command buttons and user forms, increasing team’s productivity</p>
-                </div>          
-            </li>
+                <span className='work-experience-company'>Bank of Montreal</span>
+                <span className='work-experience-location'>Toronto, ON, Canada</span>
+                <span className='work-experience-position'>Operations Analyst</span>
+                <span className='work-experience-dates'>Sep 2020 to Dec 2020</span>
+                <ul className='work-experience-description'>
+                    <li>
+                        Updated webpage used for SAP S/4HANA training resources using HTML and CSS, improving user experience by adding more resources and making page auto-scalable
+                    </li>
+                    <li>
+                        Created a General Ledger Account search engine portal using HTML, CSS and JavaScript
+                    </li>
+                    <li>
+                        Created a tool that allows users with limited HTML experience to edit the Notepad file of the SAP S/4HANA Training webpage and was nominated "Star of the Week" for it
+                    </li>
+                </ul>
+            </li>   
     </ul>
     </div>
   </div>
