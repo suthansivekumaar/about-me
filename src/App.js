ff4d0470ce3cd285f7ead003bc03edfae7dc6132
Suthan <suthan.sivekumaar@gmail.com>
2023-11-05 01:04:59 -0400

    Used hashrouter instead of browserrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import AboutMe from "./components/pages/AboutMe";
 import Experience from "./components/pages/Experience";
 import Start from "./components/pages/Start";
